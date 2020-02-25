52a038b5c3a6d98d34add52103a6227d3a276eeb
QQ1725933768 <1725933768@qq.com>
2020-02-25 17:14:22 +0800

    创建注册中心

--- consumer/src/main/java/com/eurekaconsumer/consumer/controller/FeignController.java
+++ consumer/src/main/java/com/eurekaconsumer/consumer/controller/FeignController.java
@@ -1,18 +1,19 @@
 package com.eurekaconsumer.consumer.controller;
 
 import com.eurekaconsumer.consumer.service.FeignService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
 
 @RestController
 public class FeignController {
 
-    @Autowired
+    @Resource
     private FeignService feignService;
 
     @GetMapping("/feign")
     public String getMsg(){
-        return feignService.getMsg();
+        return feignService.getMsg()+"feign";
     }
 }
