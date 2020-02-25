6598925d48007230c78eda1ec42d48ff7ee36ca9
QQ1725933768 <1725933768@qq.com>
2020-02-26 00:15:57 +0800

    创建注册中心

--- client/src/main/java/com/eurekaclient/client/service/DiscoveryService.java
+++ client/src/main/java/com/eurekaclient/client/service/DiscoveryService.java
@@ -1,9 +1,13 @@
 package com.eurekaclient.client.service;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
 public class DiscoveryService {
+
+    @Value("${eureka.client.serviceUrl.defaultZone}")
+    private String value;
 
     public String printMsg(){
         return "你好";
